0b5e0327cee706b415307b676a11813692d99982
seeminkhan@eng.rizvi.edu.in <seeminkhan@eng.rizvi.edu.in>
2024-10-26 00:58:21 +0530

    final

--- client/src/redux/constants.js
+++ client/src/redux/constants.js
@@ -2,9 +2,12 @@ let baseUrl = "https://server-y9qa.onrender.com";
 
 export const BASE_URL = baseUrl;
 
-export const USERS_URL = "/api/users";
-export const CATEGORY_URL = "/api/category";
-export const PRODUCT_URL = "/api/products";
-export const UPLOAD_URL = "/api/upload";
-export const ORDERS_URL = "/api/orders";
-export const PAYPAL_URL = "/api/config/paypal";
+export const USERS_URL = `${baseUrl}/api/users`;
+export const CATEGORY_URL = `${baseUrl}/api/category`;
+export const PRODUCT_URL = `${baseUrl}/api/products`;
+export const UPLOAD_URL = `${baseUrl}/api/upload`;
+export const ORDERS_URL = `${baseUrl}/api/orders`;
+export const PAYPAL_URL = `${baseUrl}/api/config/paypal`;
+
+// Use this for constructing URLs to access uploaded files
+export const getUploadUrl = (filename) => `${baseUrl}/uploads/${filename}`;
